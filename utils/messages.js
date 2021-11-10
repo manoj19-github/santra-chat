9088e88e867c7622dd42347db52bd1cd66043db3
Manoj Santra <santramanoj1997@gmail.com>
2021-11-11 03:16:41 +0530

    third commit

--- utils/messages.js
+++ utils/messages.js
@@ -3,7 +3,7 @@ function formatMessage(username,text){
   return{
     username,
     text,
-    time:moment().format("DD MMM YYYY h:mm a")
+    time:moment(new Date()).format("DD MMM YYYY h:mm a")
   }
 }
 module.exports=formatMessage
